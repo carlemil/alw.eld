4113477e4f6a502a8c7d75388aea86a29db7cc73
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-11-04 10:07:21 +0100

    in the middle.

--- app/src/main/java/se/kjellstrand/awp/eld/EldGenerator.java
+++ app/src/main/java/se/kjellstrand/awp/eld/EldGenerator.java
@@ -12,30 +12,65 @@ import android.renderscript.ScriptIntrinsicConvolve3x3;
  */
 public class EldGenerator {
 
+    Element elementF32;
+    Element elementRGBA;
     private Bitmap bitmap;
-
-    private Allocation allocation;
-
+    private Allocation allocationU8in;
+    private Allocation allocationU8out;
     private ScriptIntrinsicConvolve3x3 scriptIntrinsicConvolve3x3;
+    private int width;
+    private int height;
 
-    public EldGenerator(Context context, int width, int height){
+    public EldGenerator(Context context, int width, int height) {
+        this.width = width;
+        this.height = height;
 
         RenderScript rs = RenderScript.create(context, RenderScript.ContextType.DEBUG);
         rs.setPriority(RenderScript.Priority.LOW);
-        Element element = Element.U8(rs);
 
-        scriptIntrinsicConvolve3x3 = ScriptIntrinsicConvolve3x3.create(rs, element);
+        elementF32 = Element.F32(rs);
+        elementRGBA = Element.RGBA_8888(rs);
+
+        scriptIntrinsicConvolve3x3 = ScriptIntrinsicConvolve3x3.create(rs, elementF32);
 
         float[] matrix = new float[]{
-                0f,0f,0f,
-                1f,1f,1f,
-                0f,1f,0f};
+                0f, 0f, 0f,
+                1f, 1f, 1f,
+                0f, 1f, 0f};
         scriptIntrinsicConvolve3x3.setCoefficients(matrix);
 
-        //allocation = Allocation.;
+        allocationU8in = Allocation.createSized(rs, elementF32, (width * height));
+        allocationU8out = Allocation.createSized(rs, elementF32, (width * height));
     }
 
-    public Bitmap getEldadBitmap(){
+
+    public Bitmap getEldadBitmap() {
+        renderEld();
+
+        renderColors();
+
+        swapAllocations();
+
         return bitmap;
+    }
+
+    private void renderColors() {
+    int size = width* height;
+        for(int i=0; i < size; i++){
+
+        }
+
+    }
+
+    private void renderEld() {
+        scriptIntrinsicConvolve3x3.setInput(allocationU8in);
+        scriptIntrinsicConvolve3x3.forEach(allocationU8out);
+    }
+
+    private void swapAllocations() {
+        // Swap buffers
+        Allocation tmp = allocationU8in;
+        allocationU8in = allocationU8out;
+        allocationU8out = tmp;
     }
 }
