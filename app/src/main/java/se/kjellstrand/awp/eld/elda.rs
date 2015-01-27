97c35cd1c5b3ff15f029453ecf874ba956321f91
Carl-Emil Kjellstrand <erbsman@gmail.com>
2015-01-27 21:39:51 +0100

    corrected fire calculation, now movement is faster as it should be.

--- app/src/main/java/se/kjellstrand/awp/eld/elda.rs
+++ app/src/main/java/se/kjellstrand/awp/eld/elda.rs
@@ -10,11 +10,12 @@ int fadeOutSpeed = 0;
 
 void root(const int *in, int *out, uint32_t x, uint32_t y) {
 	int pos = x + y * width;
-	if(pos > 0 && pos < width * (height - 1)){
-		const int v10 = *(const int*)rsGetElementAt(inAllocation, pos - 1);
-		const int v12 = *(const int*)rsGetElementAt(inAllocation, pos + 1);
-		const int v21 = *(const int*)rsGetElementAt(inAllocation, pos + width);
-		int v = (*in + v10 + v12 + v21 - fadeOutSpeed)>>2;
+	if(pos > 0 && pos < width * (height - 2)){
+		const int v10 = *(const int*)rsGetElementAt(inAllocation, pos - 1 + width);
+		const int v11 = *(const int*)rsGetElementAt(inAllocation, pos + width);
+		const int v12 = *(const int*)rsGetElementAt(inAllocation, pos + 1 + width);
+		const int v21 = *(const int*)rsGetElementAt(inAllocation, pos + width + width);
+		int v = (v10 + v11 + v12 + v21 - fadeOutSpeed)>>2;
 		if( v < 1 ) {
 			v = 0;
 		}	
